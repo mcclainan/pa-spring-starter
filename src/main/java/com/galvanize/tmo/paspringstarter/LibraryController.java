6223d775a878fa23ee738d49e5af7c638ad56468
AMcclai13 <AMcclai13@TMW18332179.gsm1900.org>
2021-08-10 15:43:12 -0500

    Completed all stories

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,50 @@
 package com.galvanize.tmo.paspringstarter;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import data.Book;
+import data.BookContract;
+import data.Books;
 
 @RestController
 public class LibraryController {
-
-    @GetMapping("/health")
+	
+	private Books books = new Books();
+	private Gson gson = new Gson();
+	
+	@GetMapping("/health")
     public void health() {
 
+    }
+    
+    @PostMapping(path = "/api/books", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> addBook(@RequestBody Book newBook) {
+    	System.out.println(newBook);
+    	Book book = this.books.addBook(newBook);
+    	return new ResponseEntity<>(this.gson.toJson(book),HttpStatus.CREATED);
+    }
+    
+    @GetMapping(path = "/api/books", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getBooks(){
+    	BookContract contract = new BookContract(this.books.getSortedBooks());
+    	System.out.print(gson.toJson(contract));
+    	return new ResponseEntity<>(gson.toJson(contract),HttpStatus.OK);
+    }
+    
+    @DeleteMapping(path = "/api/books")
+    public ResponseEntity<String> deleteBooks(){
+    	this.books.deleteBooks();
+    	return new ResponseEntity<String>("", HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/com/galvanize/tmo/paspringstarter/PaSpringStarterApplicationTests.java
+++ src/test/java/com/galvanize/tmo/paspringstarter/PaSpringStarterApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +26,37 @@ class PaSpringStarterApplicationTests {
 	void isHealthy() throws Exception {
 		mockMvc.perform(get("/health"))
 				.andExpect(status().isOk());
+	}
+	
+	@Test
+	void testEndPoints() throws Exception{
+		String body ="{\"author\": \"Douglas Adams\",\"title\": \"The Hitchhiker's Guide to the Galaxy\",\"yearPublished\": 1979}";
+		mockMvc.perform(post("/api/books").contentType(MediaType.APPLICATION_JSON).content(body))
+		.andExpect(status().isCreated())
+		.andExpect(content().contentType("application/json"))
+		.andExpect(jsonPath("$.id").value("1"))
+		.andExpect(jsonPath("$.author").value("Douglas Adams"))
+		.andExpect(jsonPath("$.title").value("The Hitchhiker's Guide to the Galaxy"))
+		.andExpect(jsonPath("$.yearPublished").value("1979")
+		);
+	 
+		body ="{\"author\": \"Philip K. Dick\",\"title\": \"Do Androids Dream of Electric Sheep?\",\"yearPublished\": 1968}";
+		mockMvc.perform(post("/api/books").contentType(MediaType.APPLICATION_JSON).content(body))
+		.andExpect(status().isCreated());
+		
+		body ="{\"author\": \"William Gibson\",\"title\": \"Neuromancer\",\"yearPublished\": 1984}";
+		mockMvc.perform(post("/api/books").contentType(MediaType.APPLICATION_JSON).content(body))
+		.andExpect(status().isCreated());
+		
+		
+		mockMvc.perform(get("/api/books"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.books.[0].title").value("Do Androids Dream of Electric Sheep?"))
+		.andExpect(jsonPath("$.books.[1].title").value("Neuromancer"))
+		.andExpect(jsonPath("$.books.[2].title").value("The Hitchhiker's Guide to the Galaxy"));
+ 
+	
+		mockMvc.perform(delete("/api/books"))
+		.andExpect(status().isNoContent());
 	}
 }
